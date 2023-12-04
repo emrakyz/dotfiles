0eb454dc561e14f25149a456880a8fdaa5fa1eb3
Emre AKYÜZ <emreakyuz_2@hotmail.com>
2023-12-04 10:10:22 +0300

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -6,29 +6,11 @@ mount /dev/nvme0n1p2 /mnt/gentoo
 
 cd /mnt/gentoo
 
-wget https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20230319T170303Z/stage3-amd64-nomultilib-openrc-20230319T170303Z.tar.xz
+wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20231126T163200Z/stage3-amd64-nomultilib-openrc-20231126T163200Z.tar.xz
 
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 
 rm -rf stage3-*.tar.xz
-
-cd /mnt/gentoo/etc/portage
-rm -rf make.conf package.use package.mask package.accept_keywords
-curl -LO raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/make.conf
-curl -LO raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/package.accept_keywords
-curl -LO raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/package.use
-curl -LO raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/package.env
-cd /mnt/gentoo/etc/portage/profile
-rm -rf use.mask package.unmask
-curl -LO raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/use.mask
-curl -LO raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/package.unmask
-mkdir --parents /mnt/gentoo/etc/portage/env
-cd /mnt/gentoo/etc/portage/env
-curl -LO https://raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/env/clang-firefox
-curl -LO https://raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/env/clang-thinlto
-curl -LO https://raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/env/compiler-clang-nolto
-curl -LO https://raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/env/compiler-clang-vapour
-curl -LO https://raw.githubusercontent.com/emrakyz/dotfiles/main/Portage/env/compiler-clang
 
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 

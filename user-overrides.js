73137ec54814aa1481e21dcf5a2368a673df646c
Emre AKYÜZ <emreakyuz_2@hotmail.com>
2024-01-31 06:04:32 +0300

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,19 +1,13 @@
-# My Original Settings #
-user_pref("privacy.resistFingerprinting.letterboxing", false);
+// My Original Settings 
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("full-screen-api.ignore-widgets", true);
-user_pref("geo.enabled", false);
-user_pref("media.peerconnection.enabled", false);
-user_pref("media.peerconnection.ice.default_address_only", true);
 user_pref("browser.tabs.tabmanager.enabled", false);
 user_pref("browser.uidensity", 1);
 user_pref("extensions.unifiedExtensions.enabled", true);
 user_pref("ui.prefersReducedMotion", 1);
 user_pref("dom.ipc.processCount", 16);
-# Third Party Images (0-1-2)#
-user_pref("network.http.referer.XOriginPolicy", 0);
 
-# Standard Settings #
+// Standard Settings
 user_pref("browser.toolbars.bookmarks.visibility", false);
 user_pref("accessibility.typeaheadfind", false);
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
@@ -46,3 +40,101 @@ user_pref("permissions.default.camera", 2);
 user_pref("permissions.default.geo", 2);
 user_pref("permissions.default.microphone", 2);
 user_pref("pdfjs.disabled", true);
+
+// Fastfox
+user_pref("content.notify.interval", 100000);
+user_pref("gfx.canvas.accelerated.cache-items", 4096);
+user_pref("gfx.canvas.accelerated.cache-size", 512); 
+user_pref("gfx.content.skia-font-cache-size", 20);
+user_pref("browser.cache.jsbc_compression_level", 3);
+user_pref("media.memory_cache_max_size", 65536);
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.cache_resume_threshold", 3600);
+user_pref("image.mem.decode_bytes_at_a_time", 32768);
+user_pref("network.buffer.cache.size", 262144); 
+user_pref("network.buffer.cache.count", 128);
+user_pref("network.http.max-connections", 1800); 
+user_pref("network.http.max-persistent-connections-per-server", 10); 
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.dns.max_high_priority_threads", 16);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.prefetch-next", false);
+user_pref("network.predictor.enabled", false);
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("dom.enable_web_task_scheduling", true);
+user_pref("layout.css.has-selector.enabled", true);
+user_pref("dom.security.sanitizer.enabled", true);
+
+// PeskyFox
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.display.focus_ring_on_anything", true); 
+user_pref("browser.display.focus_ring_style", 0);
+user_pref("browser.display.focus_ring_width", 0);
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
+user_pref("browser.privateWindowSeparation.enabled", false);
+user_pref("cookiebanners.service.mode", 1);
+user_pref("cookiebanners.service.mode.privateBrowsing", 1);
+user_pref("cookiebanners.service.enableGlobalRules", true);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.delay", -1);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("browser.urlbar.trending.featureGate", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.download.dir", "${HOME}/downloads");
+user_pref("browser.helperApps.showOpenOptionForPdfJS", false);
+user_pref("browser.vpn_promo.enabled", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("startup.homepage_override_url", "");
+user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
+user_pref("browser.translations.enable", false);
+user_pref("browser.translations.autoTranslate", false);
+user_pref("gfx.webrender.quality.force-subpixel-aa-where-possible", true);
+user_pref("browser.urlbar.addons.featureGate", false);
+user_pref("browser.urlbar.mdn.featureGate", false);
+user_pref("browser.urlbar.pocket.featureGate", false);
+user_pref("browser.urlbar.weather.featureGate", false);
+user_pref("media.autoplay.default", 5);
+user_pref("media.block-autoplay-until-in-foreground", true);
+user_pref("media.autoplay.blocking_policy", 2);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.startup.page", 0);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtabpage.activity-stream.logowordmark.alwaysVisible", false);
+user_pref("extensions.pocket.api"," ");
+user_pref("extensions.pocket.oAuthConsumerKey", " ");
+user_pref("extensions.pocket.site", " ");
+user_pref("extensions.pocket.showHome", false);
+user_pref("browser.download.always_ask_before_handling_new_types", false);
+user_pref("browser.download.open_pdf_attachments_inline", false);
+user_pref("pdfjs.sidebarViewOnLoad", 0);
+user_pref("browser.link.open_newwindow", 3);
+user_pref("browser.link.open_newwindow.restriction", 0);
+user_pref("dom.disable_window_move_resize", true);
+user_pref("layout.word_select.eat_space_to_next_word", false);
+user_pref("editor.word_select.delete_space_after_doubleclick_selection", true);
+user_pref("reader.parse-on-load.enabled", false);
+user_pref("layout.spellcheckDefault", 0);
+user_pref("browser.display.show_image_placeholders", false);
+
+// Smoothfox
